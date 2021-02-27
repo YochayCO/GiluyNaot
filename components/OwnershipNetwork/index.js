34ed18e7ccf3c046149c1180cb1e245af752f43d
yochayco <yochai.cohenor@gmail.com>
2021-02-27 17:57:34 +0200

    Initial design of network

--- components/OwnershipNetwork/index.js
+++ components/OwnershipNetwork/index.js
@@ -8,6 +8,12 @@ const NetworkContainer = styled.div`
   border: 1px solid lightgray;
 `;
 
+const colors = {
+  red: '#ffadad',
+  green: '#adffad',
+  purple: '#adadff',
+};
+
 export default class OwnershipNetwork extends React.Component {
   constructor(props) {
     super(props);
@@ -18,7 +24,66 @@ export default class OwnershipNetwork extends React.Component {
   componentDidMount() {
     const data = parseNetwork(this.props);
     const container = document.getElementById('mynetwork');
-    const options = {};
+    const options = {
+      layout: {
+        hierarchical: {
+          nodeSpacing: 200,
+        },
+      },
+      physics: {
+        enabled: false,
+      },
+      interaction: {
+        hover: true,
+      },
+      nodes: {
+        font: {
+          size: 15,
+        },
+      },
+      groups: {
+        person: {
+          level: 1,
+          shape: 'circle',
+          widthConstraint: 80,
+          color: colors.red,
+        },
+        big_profit_company: {
+          shape: 'box',
+          widthConstraint: 130,
+          heightConstraint: 70,
+          level: 2,
+          color: colors.green,
+        },
+        small_profit_company: {
+          shape: 'box',
+          widthConstraint: 130,
+          heightConstraint: 40,
+          level: 3,
+          color: colors.green,
+        },
+        big_comm_company: {
+          shape: 'box',
+          widthConstraint: 130,
+          heightConstraint: 70,
+          level: 2,
+          color: colors.purple,
+        },
+        small_comm_company: {
+          shape: 'box',
+          widthConstraint: 130,
+          heightConstraint: 40,
+          level: 3,
+          color: colors.purple,
+        },
+      },
+      edges: {
+        width: 2,
+        arrows: {
+          to: true,
+        },
+      },
+    };
     this.network = new Network(container, data, options);
     this.network.once('afterDrawing', () => {
       container.style.height = '85vh';
